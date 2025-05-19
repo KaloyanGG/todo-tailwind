f2bf0ec367156062431038d909cbad8e3fb8f818
Kaloyan Georgiev <81187395+KaloyanGG@users.noreply.github.com>
2025-05-19 14:00:18 +0300

    [feat] Heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Todo from "./Components/Todo";
+
 function App() {
-  return <div></div>;
+  return (
+    <main className='py-10 bg-rose-100 max-w-7xl m-auto'>
+      <Todo />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/Components/Todo.tsx
@@ -0,0 +1,29 @@
+import SearchBar from "./SearchBar";
+import TodoList from "./TodoList";
+
+const Todo = () => {
+  return (
+    <div
+      id="todo-component"
+      className="m-auto w-3xl space-y-8 rounded-xl bg-white p-8"
+    >
+      <Heading />
+      <SearchBar />
+      <TodoList />
+    </div>
+  );
+};
+
+const Heading = () => {
+  return (
+    <h1 className="text-xl font-semibold tracking-wide">
+      To-Do List
+      <img
+        className="ml-2 inline h-6 bg-white"
+        src="https://cdn.iconscout.com/icon/free/png-512/free-to-do-list-icon-download-in-svg-png-gif-file-formats--schedule-message-check-banking-and-finance-pack-business-icons-2947644.png?f=webp&w=256"
+      />
+    </h1>
+  );
+};
+
+export default Todo;
