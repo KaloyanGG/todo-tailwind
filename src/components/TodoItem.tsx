b2cbf0454ad1af99cb310569d7edc8baf244e755
Kaloyan Georgiev <kokicha.gg@gmail.com>
2025-05-21 14:23:08 +0300

    [feat] Hover effect on the cancel

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -20,9 +20,17 @@ const TodoItem = ({ id, item, handleCheck }: TodoItemProps) => {
       </p>
       <button
         type="button"
-        className="ml-auto cursor-pointer font-semibold text-[var(--gray)] hover:shadow-lg hover:shadow-black"
+        className="translate relative z-10 ml-auto cursor-pointer font-semibold text-[var(--gray)] after:absolute after:top-[-15%] after:left-[-15%] after:-z-10 after:hidden after:h-[130%] after:w-[130%] after:rounded-full after:bg-gray-100 hover:after:block"
       >
-        x
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          height="18"
+          width="18"
+          className="bg-transparent fill-[var(--gray)]"
+        >
+          <path d="M10.5859 12L2.79297 4.20706L4.20718 2.79285L12.0001 10.5857L19.793 2.79285L21.2072 4.20706L13.4143 12L21.2072 19.7928L19.793 21.2071L12.0001 13.4142L4.20718 21.2071L2.79297 19.7928L10.5859 12Z"></path>
+        </svg>
       </button>
     </div>
   );
