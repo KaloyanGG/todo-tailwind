7be2b7aab174b6b86364465fe9d02a9c3483d100
Kaloyan Georgiev <kokicha.gg@gmail.com>
2025-05-22 12:45:23 +0300

    [feat] Authentication added so only I can use it

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { TodoListContext } from "./context/todoListContext";
 import { todoListReducer } from "./context/todoListReducer";
 import Todo from "./components/Todo";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./config/firebase";
+import Login from "./components/Login";
 
 function App() {
   const [todoList, dispatch] = useReducer(todoListReducer, {});
 
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      setIsAuthenticated(!!user);
+    });
+    return () => unsub();
+  }, []);
+
+  useEffect(() => {
+    if (!isAuthenticated) return;
+
     const localStorageTodos = localStorage.getItem("todos");
 
     if (localStorageTodos) {
@@ -15,11 +29,15 @@ function App() {
         payload: JSON.parse(localStorageTodos),
       });
     }
-  }, []);
+  }, [isAuthenticated]);
 
   useEffect(() => {
+    if (!isAuthenticated) return;
     localStorage.setItem("todos", JSON.stringify(todoList));
-  }, [todoList]);
+  }, [todoList, isAuthenticated]);
+
+  if (!isAuthenticated)
+    return <Login onLogin={() => setIsAuthenticated(true)} />;
 
   return (
     <main className="flex h-svh min-h-svh bg-rose-100">
