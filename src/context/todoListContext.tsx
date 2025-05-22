4f15859c7feb2a6ed5441d3bc7036fe3760e9254
Kaloyan Georgiev <kokicha.gg@gmail.com>
2025-05-22 15:13:02 +0300

    Update todoListContext.tsx

--- src/context/todoListContext.tsx
+++ src/context/todoListContext.tsx
@@ -1,8 +1,6 @@
 import { createContext } from "react";
 import type { TodoItem } from "../components/Todo";
-import type { TodoListAction } from "./todoListReducer";
 type TodoContextValue = {
   todoList: Record<string, TodoItem>;
-  // dispatch: React.ActionDispatch<[action: TodoListAction]>;
 };
 export const TodoListContext = createContext<TodoContextValue | null>(null);
