bad7ba489758ffb9bc914b94765b48ee586d34d0
Kaloyan Georgiev <kokicha.gg@gmail.com>
2025-06-27 15:25:40 +0300

    chores

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,13 @@
-import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    // VitePWA({
-    //   registerType: "autoUpdate",
-    //   devOptions: {
-    //     enabled: true,
-    //   },
-    //   manifest: {
-    //     name: "Koko Todo App",
-    //     short_name: "Todo",
-    //     start_url: "/",
-    //     display: "standalone",
-    //     background_color: "#ffffff",
-    //     theme_color: "#f43f5e",
-    //     icons: [
-    //       {
-    //         src: "pwa-192x192.png",
-    //         sizes: "192x192",
-    //         type: "image/png",
-    //       },
-    //       {
-    //         src: "pwa-512x512.png",
-    //         sizes: "512x512",
-    //         type: "image/png",
-    //       },
-    //     ],
-    //   },
-    // }),
+    // basicSsl()
   ],
 });
